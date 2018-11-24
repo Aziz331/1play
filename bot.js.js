c9c731540379ec3902de73ed6c51e9f153194fcb
Aziz331 <44896829+Aziz331@users.noreply.github.com>
2018-11-24 22:41:23 +0700

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "2";
+const prefix = "1";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`1play   `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
